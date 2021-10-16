626c679b2e8a1dfef505836e3fd836cb6f0e1f00
Scala <scala0114@gmail.com>
2021-10-16 16:52:10 +0900

    change Server Database

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,14 @@ ALLOWED_HOSTS = ['13.124.216.96']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': 'primarypoint0=',
+        'HOST': 'ls-7da01be2e60209c7f8c0b9a8bd3ec84b15f343c0.cuiyonyqrp2z.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
