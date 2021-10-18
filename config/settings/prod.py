b2eb331cdd8416f2a6118dfdad552cd9b6705415
Scala <scala0114@gmail.com>
2021-10-18 11:08:02 +0900

    change Server Database

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -10,7 +10,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'pybo',
         'USER': 'dbmasteruser',
-        'PASSWORD': 'primarypoint0=',
+        'PASSWORD': SECRET_KEY.get('DATABASE_PASSWORD'),
         'HOST': 'ls-7da01be2e60209c7f8c0b9a8bd3ec84b15f343c0.cuiyonyqrp2z.ap-northeast-2.rds.amazonaws.com',
         'PORT': '5432',
     }
